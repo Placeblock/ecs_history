51504091f41f11aa78b0d66e143c3bfd49f3cb9f
Placeblock <felix@weglehner.info>
2026-01-17 01:04:13 +0100

    Fixed issue

--- include/ecs_history/static_entity.hpp
+++ include/ecs_history/static_entity.hpp
@@ -13,32 +13,32 @@
 #endif
 
 namespace ecs_history {
-    using static_entity_t = STATIC_ENTITY_TYPE;
+using static_entity_t = STATIC_ENTITY_TYPE;
 
-    class static_entities_t {
-        struct static_entity_container_t {
-            static_entity_t id;
-        };
-
-        struct entity_container {
-            entt::entity entt;
-        };
-
-        entt::storage<static_entity_container_t> static_entities;
-        std::map<static_entity_t, entity_container> entities;
-        static_entity_t next = 0;
-
-    public:
-        uint64_t create(entt::entity entt);
-
-        void create(entt::entity entt, static_entity_t static_entity);
-
-        entt::entity remove(static_entity_t static_entity);
-
-        [[nodiscard]] static_entity_t get_static_entity(entt::entity entt) const;
-
-        [[nodiscard]] entt::entity get_entity(static_entity_t static_entity) const;
+class static_entities_t {
+    struct static_entity_container_t {
+        static_entity_t id;
     };
+
+    struct entity_container {
+        entt::entity entt;
+    };
+
+    entt::storage<static_entity_container_t> static_entities;
+    std::map<static_entity_t, entity_container> entities;
+    static_entity_t next = UINT64_MAX / 2;
+
+public:
+    uint64_t create(entt::entity entt);
+
+    void create(entt::entity entt, static_entity_t static_entity);
+
+    entt::entity remove(static_entity_t static_entity);
+
+    [[nodiscard]] static_entity_t get_static_entity(entt::entity entt) const;
+
+    [[nodiscard]] entt::entity get_entity(static_entity_t static_entity) const;
+};
 }
 
 #endif //ECS_HISTORY_STATIC_ENTITY_HPP
